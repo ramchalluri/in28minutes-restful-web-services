8235cef06f5d405aa36462818f3ad5f0b6d5cb2c
= <=>
2022-02-15 14:51:57 +1100

    Initializing

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,53 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.Date;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+public class User {
+
+	private Integer id;
+	@Size(min = 2, message = "Name should have atleast 2 chars")
+	private String name;
+
+	@Past(message = "Date Of Birth should be in the past")
+	private Date birthDate;
+	
+	protected User() {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("User [id=%s, name=%s, birthDate=%s]", id, name, birthDate);
+	}
+	public User(Integer id, String name, Date birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+	
+	
+	
+}
